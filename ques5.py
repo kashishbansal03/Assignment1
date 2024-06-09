3ad056fba8ecd76717c93f182dc5477991e245a1
kashishbansal03 <112632578+kashishbansal03@users.noreply.github.com>
2024-06-09 22:53:15 +0530

    some changes

--- ques5.py
+++ ques5.py
@@ -1,10 +1,7 @@
-# Prompt the user to enter a string
+
 user_input = input("Enter the string you want to write to the file: ")
 
-# Specify the name of the file
 file_name = "output.txt"
-
-# Open the file in write mode and write the user's input to it
 with open(file_name, "w") as file:
     file.write(user_input)
 
